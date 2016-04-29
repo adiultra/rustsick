a87c9d43dad788d1e96a2a4b4be006263936b903
Aditya Ultra <adityaultra4@gmail.com>
2016-04-30 00:46:12 +0530

    Added counter for chances in guessing_game.rs

--- books.md
+++ books.md
@@ -1,8 +1,8 @@
 # Books :book: used in learning Rust
 
 1. The Book [The Rust Programming Language](https://doc.rust-lang.org/book/index.html)
-
+2. Rust By Example [RBE](http://rustbyexample.com)
 # References
 
 1. [Standard Documentation](https://doc.rust-lang.org/reference.html)   
-:warning: Tends to be out of date 
+:warning: Tends to be out of date
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,51 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
+
+/// GuessGame from std docs of rust
 
 fn main() {
     println!("Guess the number!");
 
+    let mut chances: u32 = 0;
+
+    // Generate and store a randowm number b/w 1<->100
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is {}", secret_number);
+    // Go into a infinite loop
+    loop {
+        println!("Chances used {}", chances);
 
-    println!("Please input your guess.");
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        // guess is a mutable empty string
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        // Get input from user
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        // Convert and shadow guess into a unsigned 32bit integer
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,         // Match num if everything is OK
+            Err(_) => continue,     // Continue even if anything != OK happens
+        };
+
+        println!("You guessed: {}", guess);
+
+        // Match the respective value of guess wrt secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You Win!");
+                println!("Total Chances used => {}", chances);
+                break;              // Break the Inf Loop and exit
+            }
+        }
+
+        chances += 1;
+    }
 }

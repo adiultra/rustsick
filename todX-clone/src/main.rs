9501cd3e98824d563c9f01885d3dd0e7a89e1a1f
Aditya Ultra <adityaultra4@gmail.com>
2016-06-08 18:54:36 +0530

    Did some crazy stuff about method and struct :smile:

--- todX-clone/src/main.rs
+++ todX-clone/src/main.rs
@@ -1,3 +1,23 @@
+struct Todo {
+    todo: &'static str,
+    index: i32,
+}
+
+impl Todo {
+    fn print_my_content(&self) {
+        println!("{0} . {1}", self.index, self.todo);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let my_todo = Todo {
+        todo: "Awesome goes amazing",
+        index: 0
+    };
+
+    println!("The Todo at {0} contains : {1}", my_todo.index, my_todo.todo);
+
+    my_todo.print_my_content();
+
 }

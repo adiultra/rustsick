70305e1b3df6c03c75d530ff6b02c71faeeffe31
Aditya Ultra <adityaultra4@gmail.com>
2016-06-10 14:16:00 +0530

    Improved the game

--- four-in-a-row/src/main.rs
+++ four-in-a-row/src/main.rs
@@ -1,12 +1,55 @@
+use std::io;
+
+/// # display
+/// displays the surface of the game
 fn display(array: &[[i32; 7]; 7]) {
-    println!("=============");
+    println!("0 1 2 3 4 5 6");
+
     for row in array {
         for element in row {
             print!("{} ", element);
         }
+
         print!("\n");
     }
+
     println!("=============");
+}
+
+fn input() -> [usize; 2] {
+    // integer is a mutable empty string
+    let mut integer = String::new();
+    let mut integer2 = String::new();
+
+    // Get input from user in integer
+    io::stdin().read_line(&mut integer)
+        .expect("Failed to read line");
+
+    io::stdin().read_line(&mut integer2)
+        .expect("Failed to read line");
+
+    // Convert and shadow(displace) integer into a 32bit integer
+    let integer: usize = match integer.trim().parse() {
+        Ok(num) => num,         // Match num if everything is OK
+        Err(_) => 0,     // Continue even if anything != OK happens
+    };
+    let integer2: usize = match integer2.trim().parse() {
+        Ok(num) => num,         // Match num if everything is OK
+        Err(_) => 0,     // Continue even if anything != OK happens
+    };
+
+    [integer, integer2]
+}
+
+fn check(indices: &[usize; 2]) -> bool {
+    let mut condition = false;
+    if 7 > indices[0] {
+        if 7 > indices[0] {
+            condition = true;
+        }
+    }
+
+    condition
 }
 
 fn main() {
@@ -20,14 +63,27 @@ fn main() {
         [0, 0, 0, 0, 0, 0, 0]
     ];
 
-    display(&surface);
+    let mut chance = 0;
 
-    surface[0][1] = 9;
 
-    display(&surface);
+    for _ in 1..10 {
+        display(&surface);
+        println!("Enter the row and coloumns");
+        let index: [usize; 2] = input();
 
-    loop {
-        break;
+        if check(&index) {
+            if chance == 0 {
+                surface[index[0]][index[1]] = 1;
+                chance = 1;
+            }
+            else {
+                surface[index[0]][index[1]] = 2;
+                chance = 0;
+            }
+        }
+        else {
+            println!("Wrong Indexes, Try again");
+        }
     }
 
 }
